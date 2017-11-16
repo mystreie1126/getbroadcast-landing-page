e96c2eb6074dfb55007f26189ebd6966f2102b05
Jian <jianqilu1126@gmail.com>
2017-11-16 06:25:46 +0000

    finishing the intro section, left with cavans stuff

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,34 @@ $(".showreel-video").hover(function(){
 		"transition-duration": "1s"
 	});
 });
+
+
+var images = document.querySelectorAll(".slide-img");
+var index = 0;
+var time = 1000;
+
+function reset(){
+	index = 0;
+	images[0].style.display = 'block';
+	for(var i = 1; i < images.length; i++){
+		images[i].style.display = 'none';
+	}
+}
+
+
+reset();
+
+
+setInterval(function(){
+	index++;
+	if(index >= images.length){
+		reset();
+	}else{
+		images[index].style.display = 'block';
+	}
+
+
+
+},1500);
+
+
