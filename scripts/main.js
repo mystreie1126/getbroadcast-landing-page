bece2a06d8eaa71bd9387234e4ab6bfdfc40b0b6
Jian <jianqilu1126@gmail.com>
2017-11-21 18:55:28 +0000

    header position fiexed

--- scripts/main.js
+++ scripts/main.js
@@ -1,49 +1,78 @@
-$(".showreel-icon").hover(function(){
-	$(".team-img").css({
-		"transform":"translateX(15%)",
-		"transition-duration": "1s"
-	});
-},function(){
-	$(".team-img").css({
-		"transform":"translateX(-1.5%)",
-		"transition-duration": "1s"
-	});
-});
-
-
 $(document).ready(function(){
+	$(".showreel-icon").hover(function(){
+		$(".team-img").css({
+			"transform":"translateX(15%)",
+			"transition-duration": "1s"
+		});
+	},function(){
+		$(".team-img").css({
+			"transform":"translateX(-1.5%)",
+			"transition-duration": "1s"
+		});
+	});
+
+	$(".showreel-icon").hover(function(){
+		$(".slogan").css({
+			"transform":"translateY(-50%)",
+			"transition-duration": ".5s"
+		});
+	},function(){
+		$(".slogan").css({
+			"transform":"translateY(50%)",
+			"transition-duration": "1s"
+		});
+	});
+
 	$('.menu').click(function(){
 		$('#main-nav').toggleClass('active');
 	});
+
+	$(window).scroll(function() {
+     if($(window).scrollTop() >= 100) {
+       // $('nav').addClass('scrolled');
+       console.log(123);
+     }
+    else {
+      // $('nav').removeClass('scrolled');
+    }
+  });
+
+
 });
 
-var images = document.querySelectorAll(".slide-img");
-var index = 0;
-var time = 1000;
-
-function reset(){
-	index = 0;
-	images[0].style.display = 'block';
-	for(var i = 1; i < images.length; i++){
-		images[i].style.display = 'none';
-	}
-}
-
-
-reset();
-
-
-setInterval(function(){
-	index++;
-	if(index >= images.length){
-		reset();
-	}else{
-		images[index].style.display = 'block';
-	}
 
 
 
-},1500);
+
+
+
+// var images = document.querySelectorAll(".slide-img");
+// var index = 0;
+// var time = 1000;
+
+// function reset(){
+// 	index = 0;
+// 	images[0].style.display = 'block';
+// 	for(var i = 1; i < images.length; i++){
+// 		images[i].style.display = 'none';
+// 	}
+// }
+
+
+// reset();
+
+
+// setInterval(function(){
+// 	index++;
+// 	if(index >= images.length){
+// 		reset();
+// 	}else{
+// 		images[index].style.display = 'block';
+// 	}
+
+
+
+// },1500);
 
 //adding google map 
 function initMap() {
